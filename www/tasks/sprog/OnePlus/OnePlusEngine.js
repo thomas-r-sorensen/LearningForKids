7298d50b74b62eb0c8ca04142c7ebac52f066736
Thomas Rune Sørensen <thom009a@easv365.dk>
2016-04-27 12:34:26 +0200

    Added JSON file
    Added JSON file and changed OnePlusEngine slightly to incorporate JSON
    functionality.

--- www/tasks/sprog/OnePlus/OnePlusEngine.js
+++ www/tasks/sprog/OnePlus/OnePlusEngine.js
@@ -2,11 +2,19 @@
  * Created by Thomas on 21-04-2016.
  */
 
+var id = '';
+var request = new XMLHttpRequest();
+request.open("GET", "../../tasks/sprog/OnePlus/Tasks.json", false);
+request.send(null)
+var allTasks = JSON.parse(request.responseText);
+//alltasks.
+
+
 // the base "class" to hold our information
 // img = path to image
 // snd = path to sound
 // ms  = milliseconds before switching to next
-var AudioImage = function(img, snd, ms)
+var AudioImage = function (img, snd, ms)
 {
   this.image = img;
   this.sound = snd;
@@ -15,19 +23,19 @@ var AudioImage = function(img, snd, ms)
   this.next = null;
 
   // show this AudioImage and schedule the next one to be shown after 'showTime' milliseconds
-  this.show = function()
+  this.show = function ()
   {
     // show the image
     document.getElementById('image').src = this.image;
     // play the sound
     new Audio(this.sound).play();
 
-    if(this.next == null) return; // end of the chain; stop execution
+    if (this.next == null) return; // end of the chain; stop execution
 
     // below we jump through hoops to get past javascript's weird context rules
     temp = this;
 
-    var nxt = function()
+    var nxt = function ()
     {
       temp.next.show();
     };
@@ -35,18 +43,16 @@ var AudioImage = function(img, snd, ms)
   };
 };
 
-
-
-
 // helper function to build chains of AudioImages
-var chainAudioImages = function(/*invisible argument list*/) {
-  if(arguments.length == 0)
+var chainAudioImages = function (/*invisible argument list*/)
+{
+  if (arguments.length == 0)
   {
     return null; // no arguments given; stop execution
   }
   var first = arguments[0];
   var current = first;
-  for(var i = 1; i < arguments.length; i++) // notice i=1, so we skip first element
+  for (var i = 1; i < arguments.length; i++) // notice i=1, so we skip first element
   {
     current.next = arguments[i];
     current = current.next;
@@ -54,12 +60,29 @@ var chainAudioImages = function(/*invisible argument list*/) {
   return first;
 };
 
+
+var setId = function (fetchedURL)
+{
+  url = fetchedURL;
+  id = url.substring(url.lastIndexOf("id=") + 1);
+}
+
+var showChallenge = function ()
+{
+  //TODO
+}
+
+
+var chain = chainAudioImages(new AudioImage(allTasks.tasks[id - 1].image1, allTasks.tasks[id-1].aud1, allTasks.tasks[id-1].ms1),
+  new AudioImage(allTasks[id].image2, allTasks[id].aud2, allTasks[id].ms2),
+  new AudioImage(allTasks[id].image3, allTasks[id].aud3, allTasks[id].ms3));
+
+
 // building our chain
-var chain = chainAudioImages(new AudioImage('../../img/BlueTriangle.png', '../../aud/Trekant.mp3', 1000),
-  new AudioImage('../../img/RedCircle.png', '../../aud/Cirkel.mp3', 2000),
-  new AudioImage('../../img/GreenSquare.png', '../../aud/Firkant.mp3', 3000)); // ms=3000 for the last one doesn't matter, as it's "next" will be null
+// ms=3000 for the last one doesn't matter, as it's "next" will be null
 
 // function to call when document is loaded
-var init = function() {
+var init = function ()
+{
   chain.show();
 };
